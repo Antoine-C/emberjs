a983bd47c515f57e79640bd09d812085788f3f3a
AntoineC <coutellier.antoine@gmail.com>
2015-09-20 12:48:17 +0200

    Get only new elements on homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        return this.store.find('dribbles');
+        return this.store.filter('dribbles', {isNew: true}, function(dribbles) {
+          return dribbles.get('isNew');
+        });
     }
 });
