a77f953d311396fc0f934a9fdb28315a46a46e55
AntoineC <coutellier.antoine@gmail.com>
2015-09-22 23:42:03 +0200

    Update views

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
+        var self = this;
         return this.store.filter('dribbles', {isNew: true}, function(dribbles) {
           return dribbles.get('isNew');
         });
